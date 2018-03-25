8f5fdc46c116c96821e5b85a6552fdcca9699dcc
Pablo <Pablo@DESKTOP-NOQDKC1>
2018-03-25 20:52:26 +0200

    Cambiado el codigo de manual a utilizando librerias propias de Weka

--- GetRaw/src/ConversorTXT.java
+++ GetRaw/src/ConversorTXT.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
 
 public class ConversorTXT {
 
@@ -20,28 +24,17 @@ public class ConversorTXT {
 	public static void convertirTXTaARFF(String pRutaTXT, String pRutaARFF) {
 
 		try {
-
-			String cabecera = "@relation spam\n\n@attribute text string\n@attribute Class {spam,ham}\n";
-			String datos = "\n@data";
-			ArrayList<String> array = arrayDeLineas(pRutaTXT);
-
-			for (String linea : array) {
-				datos = datos + "\n'" + getTexto(linea).replace("'", "") + "', '" + getClase(linea) + "'";
-			}
-
-			exportarTextoArchivo(cabecera + datos, pRutaARFF);
+			
+			ArrayList<String> lineas = arrayDeLineas(pRutaTXT);
+			Instances data = convertirAInstancias(lineas);
+			exportarARFF(data, pRutaARFF);
+			
 		}
 
 		catch (Exception e) {
 			System.out.println("Se ha producido un error al convertir el TXT a ARFF");
 		}
 
-	}
-
-	private static void exportarTextoArchivo(String pTexto, String pRuta) throws Exception {
-		BufferedWriter f = new BufferedWriter(new FileWriter(pRuta, true));
-		f.write(pTexto);
-		f.close();
 	}
 
 	private static ArrayList<String> arrayDeLineas(String pRutaArchivo) throws Exception {
@@ -70,7 +63,36 @@ public class ConversorTXT {
 		while (pLinea.charAt(i) != '\t') {
 			i++;
 		}
-		return pLinea.substring(i + 1);
+		return pLinea.substring(i + 1).replace("'", "\'");
 	}
-
+	
+	private static Instances convertirAInstancias(ArrayList<String> pInstancias) {
+		
+		ArrayList<String> valores = new ArrayList<String>();
+		valores.add("spam");
+		valores.add("ham");		
+		Attribute texto = new Attribute("text", true);
+		Attribute clase = new Attribute("class", valores);	
+		ArrayList<Attribute> atributos = new ArrayList<Attribute>();	
+		atributos.add(texto);
+		atributos.add(clase);
+		Instances aDevolver = new Instances ("spam", atributos, pInstancias.size());
+		
+		for (String ins:pInstancias) {
+			Instance i = new DenseInstance(2);
+			i.setValue(texto, getTexto(ins));
+			i.setValue(clase, getClase(ins));
+			aDevolver.add(i);
+		}
+		
+		return aDevolver;
+	}
+	
+	private static void exportarARFF(Instances pData, String pRuta) throws Exception{
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(pData);
+		saver.setFile(new File(pRuta));
+		saver.writeBatch();
+	}
+	
 }
