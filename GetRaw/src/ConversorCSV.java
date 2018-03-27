8914503cec891d72680e38134d98d8691c278d0b
oscar <oscar@PORTATIL-ASUS>
2018-03-27 17:06:48 +0200

    Comentado y añadida FileNotFoundException

--- GetRaw/src/ConversorCSV.java
+++ GetRaw/src/ConversorCSV.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 
@@ -11,6 +12,7 @@ public class ConversorCSV implements IConversor {
 
 	private static ConversorCSV miConversorCSV = new ConversorCSV();
 
+	// Expresion regular que se utiliza para detectar el patron: ","
 	private final String regEx = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
 
 	private ConversorCSV() {
@@ -23,49 +25,70 @@ public class ConversorCSV implements IConversor {
 	@Override
 	public void convertir(String pathOrigen, String pathDestino) throws Exception {
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(pathOrigen))));
+		try {
+			System.out.println("Convirtiendo el fichero .csv a .arff ...");
 
-		File tempFile = new File(new File(pathOrigen).getName() + ".csv");
-		FileWriter writer = new FileWriter(tempFile);
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(new FileInputStream(new File(pathOrigen))));
 
-		String line;
-		String[] instanciaElemento;
-		String scapedText;
-		Boolean salir = false;
-		int contIntentos = 0;
+			File tempFile = new File(new File(pathOrigen).getName() + ".csv");
+			FileWriter writer = new FileWriter(tempFile);
 
-		while (!salir) {
-			line = reader.readLine();
-			if (line != null && !line.isEmpty()) {
-				contIntentos = 0;
-				instanciaElemento = line.split(regEx);
+			String line;
+			String[] instanciaElemento;
+			String scapedText;
+			Boolean salir = false;
+			int contIntentos = 0;
 
-				if (instanciaElemento.length == 5) {
+			while (!salir) {
+				line = reader.readLine();
+				if (line != null && !line.isEmpty()) {
+					contIntentos = 0;
+					instanciaElemento = line.split(regEx);
 
-					// Se limpia la linea
-					scapedText = "\"" + instanciaElemento[4].replaceAll("\"", "").replaceAll("—", "-") + "\"";
+					if (instanciaElemento.length == 5) {
 
-					// Se annade la linea al fichero temporal
-					writer.write(instanciaElemento[0] + "," + instanciaElemento[1] + "," + instanciaElemento[2] + ","
-							+ instanciaElemento[3] + "," + scapedText + "\n");
+						// Se limpia la linea
+						scapedText = "\"" + instanciaElemento[4].replaceAll("\"", "").replaceAll("—", "-") + "\"";
+
+						// Se annade la linea al fichero temporal
+						writer.write(instanciaElemento[0] + "," + instanciaElemento[1] + "," + instanciaElemento[2]
+								+ "," + instanciaElemento[3] + "," + scapedText + "\n");
+					}
+
+				} else {
+					contIntentos++;
+					if (contIntentos == 3) {
+						salir = true;
+					}
 				}
-			} else {
-				contIntentos++;
-				if (contIntentos == 3) {
-					salir = true;
-				}
+
 			}
 
+			reader.close();
+			writer.close();
+
+			Instances data = cargarCSV(tempFile);
+			exportarARFF(data, pathDestino);
 		}
 
-		reader.close();
-		writer.close();
-
-		Instances data = cargarCSV(tempFile);
-		exportarARFF(data, pathDestino);
+		catch (FileNotFoundException e) {
+			System.err.println("No se ha encontrado el fichero .csv en la ruta especificada: " + pathOrigen);
+			throw e;
+		} catch (Exception e) {
+			System.err.println("Se ha producido un error al convertir el CSV a ARFF");
+			throw e;
+		}
 
 	}
 
+	/**
+	 * Dado un fichero csv, devuelve los datos contenidos en este
+	 * 
+	 * @param pFile
+	 * @return Instances
+	 * @throws Exception
+	 */
 	private Instances cargarCSV(File pFile) throws Exception {
 		CSVLoader loader = new CSVLoader();
 		loader.setSource(pFile);
