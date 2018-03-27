fc5b7896c1dabdfa9098c183d7a1a98667f95a8d
oscar <oscar@PORTATIL-ASUS>
2018-03-27 15:46:40 +0200

    Javadoc añadido

--- GetRaw/src/IConversor.java
+++ GetRaw/src/IConversor.java
@@ -5,8 +5,23 @@ import weka.core.converters.ArffSaver;
 
 public interface IConversor {
 
+	/**
+	 * Dado una ruta de origen y una de destino, convierte lo que haya en la ruta de
+	 * origen a fichero .arff en la ruta de destino.
+	 * 
+	 * @param pathOrigen
+	 * @param pathDestino
+	 * @throws Exception
+	 */
 	void convertir(String pathOrigen, String pathDestino) throws Exception;
 
+	/**
+	 * Genera un fichero .arff en la ruta indicada.
+	 * 
+	 * @param pData
+	 * @param pRuta
+	 * @throws Exception
+	 */
 	default void exportarARFF(Instances pData, String pRuta) throws Exception {
 		ArffSaver saver = new ArffSaver();
 		saver.setInstances(pData);
